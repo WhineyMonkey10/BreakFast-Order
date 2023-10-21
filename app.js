4c86f1ca9a877ce39a6644a0d07877ebafdfc4a4
WhineyMonkey10 <gbialystokjoly@gmail.com>
2023-10-21 18:15:23 +0200

    Turns out it wasn't the final commit before. Added sorting by item type on the user frotnend.

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Set up MongoDB connection
-mongoose.connect("uri", {
+mongoose.connect("", {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
